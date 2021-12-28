465d09f3994691d4e73aa8723dfc3beed6f2d725
YourHarbour <49119684+YourHarbour@users.noreply.github.com>
2021-12-28 16:51:12 +1100

    Add new pages and components of ETS (#1)
    * Add Home Page and searchByDevice page
    
    * Add external files
    
    * small fix
    
    * small fix
    
    * Add how to run
    
    * Remove unnecessary files
    
    * Add external files
    
    * small fix
    
    * small fix
    
    * Add how to run
    
    * Remove unnecessary files
    
    * Resolve conflicts
    
    Co-authored-by: Harbour Zhang <bzha6798@uni.sydney.edu.au>

---
+++ eco_system_track/src/components/banner/Banner.js
@@ -0,0 +1,68 @@
+import { Layout, Menu } from 'antd';
+import React from "react";
+import 'antd/dist/antd.css';
+import {Link} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { GlobalOutlined } from '@ant-design/icons';
+import i18next from 'i18next'
+import {useTranslation} from "react-i18next";
+
+const { Header } = Layout;
+
+//Category means different mode name/banner render stuff
+const categories = [
+    {
+        'url': 'searchByDevice'
+    },
+]
+
+//Support languages, add to make dropdown more choice
+//code: the language code in ISO 639-1
+//language: the display name of the language in dropdown
+const languages = [
+    {
+        'code':'en',
+        "language":"English"
+    },
+    {
+        'code':'zh-Hans',
+        "language":"简体中文"
+    }
+]
+
+const { SubMenu } = Menu;
+const Banner = () => {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+    return(
+        <div>
+            <Header style={{background:'#fff'}}>
+                <Menu mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Link to={"/"}>
+                        <Menu.Item>
+                            <img width="200" height="62" alt="RISC-V International"
+                                 src="https://riscv.org/wp-content/uploads/2020/06/riscv-color.svg"
+                                 srcSet="https://riscv.org/wp-content/uploads/2020/06/riscv-color.svg 1x, https://riscv.org/wp-content/uploads/2020/06/riscv-color.svg 2x" />
+                        </Menu.Item>
+                    </Link>
+                    {categories.map((category) => {
+                        const key = category.url;
+                        const url = '/' + key;
+                        return <Menu.Item key={key} onClick={() => {navigate(url)}}>{t(key)}</Menu.Item>;
+                    })}
+                    <SubMenu key="SubMenu" style={{marginLeft:"auto"}} icon={<GlobalOutlined />} title={t('language')}>
+                        {
+                            languages.map((language)=>{
+                                const lan = language.language;
+                                const code = language.code;
+                                return <Menu.Item key={code} onClick={()=>{i18next.changeLanguage(code)}}>{lan}</Menu.Item>
+                            })
+                        }
+                    </SubMenu>
+                </Menu>
+            </Header>
+        </div>
+    )
+}
+
+export default Banner;
